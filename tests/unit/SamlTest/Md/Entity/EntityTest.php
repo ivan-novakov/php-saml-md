0009911c8dde5f075e1b7a3d2cd0d8b9aed30e05
Ivan Novakov <ivan.novakov@debug.cz>
2013-03-31 09:06:13 +0200

    added entity tests

--- tests/unit/SamlTest/Md/Entity/EntityTest.php
+++ tests/unit/SamlTest/Md/Entity/EntityTest.php
@@ -35,8 +35,87 @@ class EntityTest extends \PHPUnit_Framework_TestCase
     }
 
 
+    public function testGetOrganizationInfo()
+    {
+        $expectedValue = array(
+            'en' => array(
+                'OrganizationName' => 'CESNET',
+                'OrganizationDisplayName' => 'CESNET, a. l. e.',
+                'OrganizationURL' => 'http://www.ces.net/'
+            ),
+            'cs' => array(
+                'OrganizationName' => 'CESNET',
+                'OrganizationDisplayName' => 'CESNET, z. s. p. o.',
+                'OrganizationURL' => 'http://www.cesnet.cz/'
+            )
+        );
+        
+        $this->assertSame($expectedValue, $this->entity->getOrganizationInfo());
+    }
+
+
+    public function testGetContactInfo()
+    {
+        $expectedValue = array(
+            array(
+                'info' => Array(
+                    'GivenName' => 'Ivan',
+                    'SurName' => 'Novakov',
+                    'EmailAddress' => 'novakoi@fel.cvut.cz'
+                ),
+                
+                'type' => 'technical'
+            ),
+            
+            array(
+                'info' => array(
+                    'GivenName' => 'Jan',
+                    'SurName' => 'Tomášek',
+                    'EmailAddress' => 'jan.tomasek@cesnet.cz'
+                ),
+                
+                'type' => 'technical'
+            )
+        );
+        
+        $this->assertSame($expectedValue, $this->entity->getContactInfo());
+    }
+
+
+    public function testGetXpathFactoryAfterSet()
+    {
+        $xpathFactory = $this->getMock('Saml\Md\Parser\XpathFactory');
+        $this->entity->setXpathFactory($xpathFactory);
+        $this->assertSame($xpathFactory, $this->entity->getXpathFactory());
+    }
+
+
+    public function testGetXpathFactoryImplicit()
+    {
+        $xpathFactory = $this->entity->getXpathFactory();
+        $this->assertInstanceOf('Saml\Md\Parser\XpathFactory', $xpathFactory);
+    }
+
+
     public function testGetXpath()
     {
         $this->assertInstanceOf('DOMXPath', $this->entity->getXpath());
+    }
+
+
+    public function testQueryXpath()
+    {
+        $xpath = $this->getMockBuilder('DomXPath')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $xpathFactory = $this->getMock('Saml\Md\Parser\XpathFactory');
+        $xpathFactory->expects($this->once())
+            ->method('createXpath')
+            ->with($this->isInstanceOf('DomDocument'))
+            ->will($this->returnValue($xpath));
+        $this->entity->setXpathFactory($xpathFactory);
+        
+        $this->assertSame($xpath, $this->entity->getXpath());
     }
 }
